985f32ae946674d721b382261de1fa2a032d829a
owaisgoldi <mohd.owais1@yahoo.com>
2022-10-05 11:45:59 +0530

    Add react app

---
+++ Server/Server.js
@@ -0,0 +1,16 @@
+const socketIO = require('socket.io')(5000);
+
+socketIO.on('connection',socket=>{
+    const id = socket.handshake.query.id;
+    socket.join(id);
+
+    socket.on('send-message', ({recipients,text})=>{
+        recipients.forEach(recipient =>{
+            const newRecipients = recipients.filter(r => r !== recipient)
+            newRecipients.push(id);
+            socket.broadcast.to(recipient).emit('receive-message', {
+                recipients: newRecipients, sender: id, text
+            })
+        })
+    })
+})
