7af8e8e9ac6c49de54dd78040867de6f36e2b49d
owaisgoldi <mohd.owais1@yahoo.com>
2022-10-05 15:42:50 +0530

    remove files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
+import { useState } from "react";
 import Login from "./components/Login";
-import { SocketProvider } from "./contexts/SocketProvider";
-import {ContactsProvider} from "./contexts/ContactsProvider";
-import {ConversationsProvider} from './contexts/ConversationsProvider';
-import useLocalStorage from "./hooks/useLocalStorage";
-import Dashboard from "./components/Dashboard";
+// import { SocketProvider } from "./contexts/SocketProvider";
+// import {ContactsProvider} from "./contexts/ContactsProvider";
+// import {ConversationsProvider} from './contexts/ConversationsProvider';
+// import useLocalStorage from "./hooks/useLocalStorage";
+// import Dashboard from "./components/Dashboard";
 
 function App() {
 
-  const [id, setId] = useLocalStorage('id')
+  const [id, setId] = useState('id');
 
-  const dashboard =  (
-    <SocketProvider id={id}>
-      <ContactsProvider>
-        <ConversationsProvider id={id}>
-          <Dashboard id={id} />
-        </ConversationsProvider>
-      </ContactsProvider>
-    </SocketProvider>
-  );
+  // const dashboard =  (
+  //   <SocketProvider id={id}>
+  //     <ContactsProvider>
+  //       <ConversationsProvider id={id}>
+  //         <Dashboard id={id} />
+  //       </ConversationsProvider>
+  //     </ContactsProvider>
+  //   </SocketProvider>
+  // );
 
   return (
     <div className="App">
